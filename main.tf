acae46b0d3203cfd8c70e1e0595df03f3b299328
alessianatale <58854390+alessianatale@users.noreply.github.com>
2023-01-18 16:31:41 +0100

    tf file config

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  # inserire i propri dati di azure
+  subscription_id   = "<azure_subscription_id>"
+  tenant_id         = "<azure_subscription_tenant_id>"
+  client_id         = "<service_principal_appid>"
+  client_secret     = "<service_principal_password>"
+}
