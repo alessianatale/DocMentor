ef14e8826ee9770651963af6c9200d70526293c5
alessianatale <58854390+alessianatale@users.noreply.github.com>
2023-03-23 10:00:53 +0100

    updates

--- bots/welcomeBot.js
+++ bots/welcomeBot.js
@@ -22,23 +22,20 @@ class WelcomeBot extends ActivityHandler {
         this.userState = userState;
 
         this.onMessage(async (context, next) => {
-            // Read UserState. If the 'DidBotWelcomedUser' does not exist (first time ever for a user)
-            // set the default to false.
+            console.log('Running dialog with Message Activity.');
+
+            //first time ever for a user
             const didBotWelcomedUser = await this.welcomedUserProperty.get(context, false);
 
-            // Your bot should proactively send a welcome message to a personal chat the first time
-            // (and only the first time) a user initiates a personal chat with your bot.
             if (didBotWelcomedUser === false) {
                 // The channel should send the user name in the 'From' object
                 const userName = context.activity.from.name;
-                await context.sendActivity('You are seeing this message because this was your first message ever sent to this bot.');
-                await context.sendActivity(`It is a good practice to welcome the user and provide personal greeting. For example, welcome ${ userName }.`);
+                await context.sendActivity(`Welcome ${ userName }.`);
+                await context.sendActivity(`Scrivi "intro"`);
 
                 // Set the flag indicating the bot handled the user's first message.
                 await this.welcomedUserProperty.set(context, true);
             } else {
-                // This example uses an exact match on user's input utterance.
-                // Consider using LUIS or QnA for Natural Language Processing.
                 const text = context.activity.text.toLowerCase();
                 switch (text) {
                 case 'hello':
@@ -65,26 +62,15 @@ class WelcomeBot extends ActivityHandler {
         this.onMembersAdded(async (context, next) => {
             // Iterate over all new members added to the conversation
             for (const idx in context.activity.membersAdded) {
-                // Greet anyone that was not the target (recipient) of this message.
-                // Since the bot is the recipient for events from the channel,
-                // context.activity.membersAdded === context.activity.recipient.Id indicates the
-                // bot was added to the conversation, and the opposite indicates this is a user.
                 if (context.activity.membersAdded[idx].id !== context.activity.recipient.id) {
-                    await context.sendActivity(`Welcome to the 'Welcome User' Bot. This bot will introduce you to welcoming and greeting users.`);
-                    await context.sendActivity(`You are seeing this message because the bot received at least one 'ConversationUpdate' ` +
-                        `event, indicating you (and possibly others) joined the conversation. If you are using the emulator, ` +
-                        `pressing the 'Start Over' button to trigger this event again. The specifics of the 'ConversationUpdate' ` +
-                        `event depends on the channel. You can read more information at https://aka.ms/about-botframework-welcome-user`);
-                    await context.sendActivity(`You can use the activity's 'locale' property to welcome the user ` +
+                    /*await context.sendActivity(`You can use the activity's 'locale' property to welcome the user ` +
                         `using the locale received from the channel. ` +
                         `If you are using the Emulator, you can set this value in Settings. ` +
-                        `Current locale is '${ context.activity.locale }'`);
-                    await context.sendActivity(`It is a good pattern to use this event to send general greeting to user, explaining what your bot can do. ` +
-                        `In this example, the bot handles 'hello', 'hi', 'help' and 'intro'. ` +
-                        `Try it now, type 'hi'`);
+                        `Current locale is '${ context.activity.locale }'`);*/
+                        await context.sendActivity(`Benvenuto, questo bot fa queste cose: .........`);
+                        await context.sendActivity(`Scrivi qualsiasi cosa per avviare il bot`);
                 }
             }
-
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
@@ -112,9 +98,9 @@ class WelcomeBot extends ActivityHandler {
                     value: 'https://docs.microsoft.com/en-us/azure/bot-service/?view=azure-bot-service-4.0'
                 },
                 {
-                    type: ActionTypes.OpenUrl,
-                    title: 'Ask a question',
-                    value: 'https://stackoverflow.com/questions/tagged/botframework'
+                    type: ActionTypes.ImBack,
+                    title: 'Genera ID',
+                    value: 'ecco il tuo id: ' + context.activity.from.id
                 },
                 {
                     type: ActionTypes.OpenUrl,
